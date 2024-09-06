dfbb64b09a12a64dfe9667f3effc3e1723cfa8f4
phil-qa <104486392+phil-qa@users.noreply.github.com>
2024-09-06 16:31:36 +0100

    Lab-10 useEffect

---
+++ effect/src/App.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import "./App.css";
+import { useState } from "react";
+import AddConfetti from "./components/AddConfetti";
+
+function App() {
+  const [isConfettiEnabled, setIsConfettiEnabled] = useState(true);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    AddConfetti({ text: count.toString() });
+  }, [count]);
+
+  return (
+    <main>
+      <h1>Confetti Counter</h1>
+      <button onClick={() => setIsConfettiEnabled((previous) => !previous)}>
+        Confetti:{isConfettiEnabled ? "On" : "Off"}{" "}
+      </button>
+      <h2>{count}</h2>
+      <button onClick={() => setCount((previous) => previous + 1)}>+1</button>
+    </main>
+  );
+}
+
+export default App;
---
+++ effect/src/components/AddConfetti.jsx
@@ -0,0 +1,13 @@
+import JSConfetti from "js-confetti";
+
+const jsConfetti = new JSConfetti();
+
+const addConfetti = ({ text }) => {
+  jsConfetti.addConfetti({
+    emojis: [text],
+    confettiNumber: 12,
+    emojiSize: 50,
+  });
+};
+
+export default addConfetti;
