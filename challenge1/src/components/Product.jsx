cdb29a08440b25e060c221bf8b29b243fbd4e24d
phil-qa <104486392+phil-qa@users.noreply.github.com>
2024-09-06 12:23:08 +0100

    Challenge - 2 on the Shop app

--- challenge1/src/components/Product.jsx
+++ challenge1/src/components/Product.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Product = ({ name, price, imageUrl }) => {
+  const [addToCart, setAddToCart] = useState(false);
+
+  const handleAddClick = () => {
+    setAddToCart((prevState) => !prevState);
+  };
+
   return (
     <div className="card h-100">
       <img src={imageUrl} alt={name} className="card-img-top" />
       <div className="card-body d-flex flex-column">
         <h4 className="card-title">{name}</h4>
         <p className="card-text">£ {price}</p>
-        <button className="btn btn-light mt-auto">Add to cart</button>
+        <button
+          className={`btn mt-auto ${addToCart ? "btn-success" : "btn-light"}`}
+          onClick={handleAddClick}
+        >
+          {addToCart ? "Added to cart" : "Add To Cart"}
+        </button>
       </div>
     </div>
   );
